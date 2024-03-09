3a735360940002ec50867291e568cb3e82970a31
Pawan <pk.it050101@gmail.com>
2024-03-09 21:02:49 +0530

    Developing the Model

--- mart_app/models.py
+++ mart_app/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator, MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
+
+STATE_CHOICES = (
+    ('AN', 'Andaman and Nicobar Islands'),
+    ('AP', 'Andhra Pradesh'),
+    ('AR', 'Arunachal Pradesh'),
+    ('AS', 'Assam'),
+    ('BR', 'Bihar'),
+    ('CH', 'Chandigarh'),
+    ('CT', 'Chhattisgarh'),
+    ('DN', 'Dadra and Nagar Haveli and Daman and Diu'),
+    ('DL', 'Delhi'),
+    ('GA', 'Goa'),
+    ('GJ', 'Gujarat'),
+    ('HR', 'Haryana'),
+    ('HP', 'Himachal Pradesh'),
+    ('JK', 'Jammu and Kashmir'),
+    ('JH', 'Jharkhand'),
+    ('KA', 'Karnataka'),
+    ('KL', 'Kerala'),
+    ('LD', 'Lakshadweep'),
+    ('MP', 'Madhya Pradesh'),
+    ('MH', 'Maharashtra'),
+    ('MN', 'Manipur'),
+    ('ML', 'Meghalaya'),
+    ('MZ', 'Mizoram'),
+    ('NL', 'Nagaland'),
+    ('OR', 'Odisha'),
+    ('PY', 'Puducherry'),
+    ('PB', 'Punjab'),
+    ('RJ', 'Rajasthan'),
+    ('SK', 'Sikkim'),
+    ('TN', 'Tamil Nadu'),
+    ('TG', 'Telangana'),
+    ('TR', 'Tripura'),
+    ('UP', 'Uttar Pradesh'),
+    ('UT', 'Uttarakhand'),
+    ('WB', 'West Bengal'),
+)
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zipcode = models.ImageField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=200)
+
+    def __str__(self):
+        return str(self.id)
+
+
+CATEGORY_CHOICES = (
+    ('mobile', 'Mobile'),
+    ('laptop', 'Laptop'),
+    ('top_wear', 'Top Wear'),
+    ('bottom_wear', 'Bottom Wear'),
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='productimg')
+
+    def __str__(self):
+        return str(self.id)
+
+
