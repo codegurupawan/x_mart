edd23f2a58b8eb176df4e12c1e357ee730045231
Pawan <pk.it050101@gmail.com>
2024-03-09 17:51:25 +0530

    Development statrted for XMart App

---
+++ mart_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MartAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mart_app'
---
+++ mart_app/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from mart_app import views
+
+
+urlpatterns = [
+    path('', views.home),
+    path('product-detail/', views.product_detail, name='product-detail'),
+    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('buy/', views.buy_now, name='buy-now'),
+    path('profile/', views.profile, name='profile'),
+    path('address/', views.address, name='address'),
+    path('orders/', views.orders, name='orders'),
+    path('change-password/', views.change_password, name='changepassword'),
+    path('mobile/', views.mobile, name='mobile'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.customer_registration, name='customerregistration'),
+    path('checkout/', views.checkout, name='checkout'),
+]
+
